9e6298e227ebde635aac89a00d7818f2669e59ec
Abdimaalik Sahal <abdimaaaalik0@gmail.com>
2026-03-29 18:15:20 +0100

    Set up Spring Boot backend

---
+++ backend/src/main/java/com/group33/timesheet/TimesheetApplication.java
@@ -0,0 +1,12 @@
+package com.group33.timesheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimesheetApplication.class, args);
+    }
+}
---
+++ backend/src/main/java/com/group33/timesheet/TimesheetApplicationTests.java
@@ -0,0 +1,12 @@
+package com.group33.timesheet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
